391ae33613bd1d7963ac61861db7c95d45f34b50
shaloo <shaloo@newfang.io>
2024-08-08 16:00:52 +0530

    remove imports for sandpack

--- new-gl/index.js
+++ new-gl/index.js
@@ -1,16 +1,6 @@
 
 import { myerc20abi } from "./myerc20.js";
 
-/*
-const { ethers, BrowserProvider } = await import(
-  "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.13.0/ethers.umd.min.js"
-);
-*/
-
-const { SCW } = await import(
-  "https://unpkg.com/@arcana/scw@0.0.43/dist/standalone.mjs"
-);
-
 //const clientId = "xar_dev_1ce3de98ebac47e4196a1380b61422dcf63a9c7b";
 const clientId = "xar_dev_1e3ee6a5cecc593d0dac2e1893dbe7534a174ac4";
 
