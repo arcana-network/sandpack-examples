3bf316138372f76b90995c9751ed409dadfefea8
shaloo <shaloo@newfang.io>
2024-12-30 14:14:07 +0530

    Fix version of ca-sdk dependency

--- ca/index.js
+++ ca/index.js
@@ -12,14 +12,13 @@ let provider = window.ethereum;
 
 if (!provider){
   console.log("Initialize MetaMask before running the playground");
-  return;
+  exit;
 }
 
-
-async function initCA() {
-  console.log("Initializing CA SDK");
-  ca = new CA(window.ethereum, {
-      network: "testnet",
+const initCA = () => {
+    console.log("Initializing CA SDK");
+    ca = new CA(window.ethereum, {
+        network: "testnet",
   });
  
   if (!ca){
@@ -31,14 +30,16 @@ async function initCA() {
     "CA Object created using MetaMask.";
 
   console.log("Initializing CA SDK");
-  try {
-    await ca.init();
-    console.log("CA SDK initialized!");
-    document.querySelector("#result").innerHTML =
-      "CA SDK Initialized for MetaMask. Add code for allowance, transfers, transactions via the SDK";
-  } catch (e) {
-      console.log({ e });
-  }
+  async () => {
+    try {
+      await ca.init();
+      console.log("CA SDK initialized!");
+      document.querySelector("#result").innerHTML =
+        "CA SDK Initialized for MetaMask. Add code for allowance, transfers, transactions via the SDK";
+    } catch (e) {
+        console.log({ e });
+    }
+ }
 }
 
 document.querySelector("#Btn-InitCA").addEventListener("click", initCA);
